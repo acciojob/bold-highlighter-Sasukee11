504db76380e2e18831c355d8a588bbd82a82e5a9
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-15 00:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const strongElement = document.querySelectorAll('strong');
 function highlight() {
-    //Write your code here
-
-
+  //Write your code here
+  strongElement.forEach((t) => (t.style.color = 'green'));
 }
-
 
 function return_normal() {
-    //Write your code here
-
-    
+  //Write your code here
+  strongElement.forEach((t) => (t.style.color = 'black'));
 }
+
+console.log(strongElement);
+
